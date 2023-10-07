ef8b9c980273ff4a917bbf799bfc12419a24dbb6
chengzh <chengzhenghua@liyusoft.com.cn>
2023-10-07 17:53:05 +0800

    first commit

---
+++ datas.js
@@ -0,0 +1,14 @@
+const datas = [
+  {
+    path: "./1.yuan.html",
+    title: "yuan",
+  },
+  {
+    path: "./2.eye.html",
+    title: "eye",
+  },
+  {
+    path: "./3.source.html",
+    title: "source",
+  },
+];
